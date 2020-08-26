d58cda441ce3055f5fb1a7ac09e558edbb0468bc
yongzheJIN <657647653@qq.com>
2020-08-26 15:26:41 +0800

    version5

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,8 +8,55 @@ import { ModalController, NavParams } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  jsonData:any=[];
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.initializaJSONData();
+  }
 
-  
+  FilterJSONData(ev:any)
+  {
+    this.initializaJSONData();
+    const val = ev.target.value; 
+    if (val && val.trim() != ''){
+        this.jsonData = this.jsonData.filter((item)=>{
+        return (item.name.toLowerCase().indexof(val.toLowercase())>-1);
+      })
+    }
+  }
+  selectval(val)
+  {
+    this.router.navigate(['video']);
+  }
+  initializaJSONData()
+  {
+    this.jsonData=[
+      {
+        "name":"OneVideo",
+        "code":"v1"
+      },
+      {
+        "name":"TwoVideo",
+        "code":"v2"
+      },
+      {
+        "name":"ThreeVideo",
+        "code":"v3"
+      },
+      {
+        "name":"FourVideo",
+        "code":"v4"
+      },
+      {
+        "name":"FixeVideo",
+        "code":"v5",
+      },
+      {
+        "name":"SixeVideo",
+        "code":"v6",
+      }
+  ]
 }
+}
+
+
