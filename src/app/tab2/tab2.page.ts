529609229dc7743f2e5f082c02ccf794c184f11d
allen <onlychuan95@gmail.com>
2020-08-28 17:40:27 +1000

    search bar created

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,55 +8,44 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  jsonData:any=[];
+  items;
 
   constructor(private router: Router) {
-    this.initializaJSONData();
+    this.initializeItems();
   }
 
-  FilterJSONData(ev:any)
-  {
-    this.initializaJSONData();
-    const val = ev.target.value; 
-    if (val && val.trim() != ''){
-        this.jsonData = this.jsonData.filter((item)=>{
-        return (item.name.toLowerCase().indexof(val.toLowercase())>-1);
-      })
-    }
+  initializeItems() {
+    this.items = [
+      'Andrew jin',
+      'Allen gu',
+      'rita zhu',
+      'amar',
+      'branyt xiao',
+      'jerry zhang'
+    ];
   }
+
+ 
   selectval(val)
   {
     this.router.navigate(['video']);
   }
-  initializaJSONData()
-  {
-    this.jsonData=[
-      {
-        "name":"OneVideo",
-        "code":"v1"
-      },
-      {
-        "name":"TwoVideo",
-        "code":"v2"
-      },
-      {
-        "name":"ThreeVideo",
-        "code":"v3"
-      },
-      {
-        "name":"FourVideo",
-        "code":"v4"
-      },
-      {
-        "name":"FixeVideo",
-        "code":"v5",
-      },
-      {
-        "name":"SixeVideo",
-        "code":"v6",
-      }
-  ]
-}
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  
 }
 
 
